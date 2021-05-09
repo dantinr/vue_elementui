ddf1ff690a98d1156779cfc2bb81004fb6ce1fbc
dante <dantinr@gmail.com>
2021-05-09 10:39:23 +0800

    element-ui test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import { Button, Select } from 'element-ui'
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Select)
 
 new Vue({
   router,
